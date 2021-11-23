bb7e6b242ac310fbaf005f7e62bdd90e4ac8fc2b
InvincibleRMC <rmc@carlstrom.com>
2021-11-23 15:23:00 -0500

    insertion works

--- p5_RMC170_Carlstrom/Sort.java
+++ p5_RMC170_Carlstrom/Sort.java
@@ -6,36 +6,39 @@ public class Sort {
 
     public static void test() {
         System.out.println("Started testing");
-        Sort sort = new Sort();
 
-        // mergeSort();
+        mergeSort(generateRandom());
         // quickSort();
-        sort.insertionSort(generateRandom());
+        insertionSort(generateRandom());
 
         // upgradedQuickSort();
         // select();
     }
 
-    public Sort() {
+    public static void mergeSort(int[] input){
+        System.out.println(toString(input));
 
+        
     }
 
-    public void insertionSort(int[] input) {
+    public static void insertionSort(int[] input) {
 
         System.out.println(toString(input));
 
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 1; i < input.length; i++) {
 
             int currVal = input[i];
-            int j = i;
-            while (currVal < input[j]) {
+
+            int j = i - 1;
+            while (j >= 0 && currVal < input[j]) {
+                input[j + 1] = input[j];
                 j--;
+
             }
-            int temp = input[j];
-            input[j] = currVal;
-            input[i] = temp;
+
+            input[j + 1] = currVal;
         }
-        
+
         System.out.println("Break");
         System.out.println(toString(input));
         System.out.println("Is Sorted? " + isSorted(input));
@@ -43,16 +46,18 @@ public class Sort {
 
     public static int[] generateRandom() {
         System.out.println("Generating arrays");
-        int[] random = new int[(int) (Math.random() * 100)];
+        int[] random = new int[
+        // (int) (Math.random() * 100)
+        10];
         for (int i = 0; i < random.length; i++) {
-            random[i] = i;
+            random[i] = (int) (Math.random() * 10);
         }
 
         System.out.println("Finished generating arrays");
         return random;
     }
 
-    public String toString(int[] input) {
+    public static String toString(int[] input) {
 
         StringBuilder str = new StringBuilder();
 
@@ -62,8 +67,17 @@ public class Sort {
         return str.toString();
     }
 
-    public boolean isSorted(int[] input) {
+    public static boolean isSorted(int[] input) {
         for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] > input[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(int[] input, int count) {
+        for (int i = 0; i < count; i++) {
             if (input[i] > input[i + 1]) {
                 return false;
             }
